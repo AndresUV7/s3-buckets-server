11ea40268d2804e854f4177db4edbd1e959d4528
AndresUV7 <andres.ullauri18@gmail.com>
2020-01-15 23:08:25 -0500

    primer commit

---
+++ s3rver/src/test/java/com/tests3/backend/s3rver/S3rverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tests3.backend.s3rver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S3rverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
